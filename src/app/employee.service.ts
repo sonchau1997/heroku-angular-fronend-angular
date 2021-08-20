a3f047aab0eb15f56b454290c4ef0d976b5f571c
sonchau1997 <48537554+sonchau1997@users.noreply.github.com>
2021-08-20 17:04:01 +0700

    Update employee.service.ts

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -9,8 +9,11 @@ import { map } from 'rxjs/operators';
 })
 export class EmployeeService {
 
-  private baseURL='http://localhost:8080/api/v1/employees';
-  private baseURL1="http://localhost:8080/api/v1/employees";
+//   private baseURL='http://localhost:8080/api/v1/employees';
+//   private baseURL1="http://localhost:8080/api/v1/employees";
+  https://springboot-java-backend.herokuapp.com/api/v1/employees
+  private baseURL='https://springboot-java-backend.herokuapp.com/api/v1/employees';
+  private baseURL1="https://springboot-java-backend.herokuapp.com/api/v1/employees";
 
   constructor(private httpClient:HttpClient) { }
 
